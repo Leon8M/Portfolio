c6455accaf906ae59384823aa293e690033a2690
Leon8M <leonmunene254@gmail.com>
2023-01-26 22:21:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let box = document.querySelector('.box')
+let logo = document.querySelector('.logo')
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            box.style.animation = 'splash 1s ease forwards 0.7s';
+        },400)
+   })
+      
+})  
